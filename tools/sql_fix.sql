be50dc33015d4b0c1004bb2b9f17c8774a2e3982
Bigdayi <i@dayiyi.top>
2023-10-06 14:19:30 +0800

    modified:   src/main/java/net/dabbit/skd21/exam/mapper/QuestionTemplateMapper.xml modified:   src/main/resources/application.yml modified:   src/main/resources/static/pages/template-list.html modified:   src/main/resources/static/pages/template.html modified:   tools/sql_fix.sql

--- tools/sql_fix.sql
+++ tools/sql_fix.sql
@@ -1,3 +1,8 @@
 -- 菜单表的修改和完善
 INSERT INTO `menu` (`menu_code`, `menu_name`, `menu_url`, `menu_level`, `parent_id`, `sort`) VALUES ('user-add', '新增用户', 'user-add.html', '2', 28, 2);
 UPDATE `menu` SET `menu_url` = 'user-list.html' WHERE `id` = 36;
+
+-- 菜单添加
+INSERT INTO `ovo`.`menu` (`menu_code`, `menu_name`, `menu_url`, `menu_level`, `parent_id`, `sort`) VALUES ('template-list', '模板列表', 'template-list.html', '2', '40', '1');
+-- UPDATE `ovo`.`menu` SET `menu_code` = 'template', `menu_name` = '模板管理' WHERE (`id` = '40');
+INSERT INTO `ovo`.`menu` (`menu_code`, `menu_name`, `menu_url`, `menu_level`, `parent_id`, `sort`) VALUES ('template-add', '模板新增', 'template.html', '2', '40', '2');
